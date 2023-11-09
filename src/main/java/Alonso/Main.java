905b228e7d13becd590476857bbaf34bd9e959d6
alonsoski <j.alonso01@ufromail.cl>
2023-11-09 13:14:12 -0300

    Junit agregado

---
+++ src/main/java/Alonso/Main.java
@@ -0,0 +1,7 @@
+package Alonso;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/Alonso/MainTest.java
@@ -0,0 +1,21 @@
+package Alonso;
+import org.junit.jupiter.api.Assertions;
+import static Alonso.Main.*;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    /*
+    @Test
+    void sumaTest(){
+        Assertions.assertEquals(suma(1,3),4);
+    }
+
+    void concatenarTest(){
+        Assertions.assertEquals(concatenar("a","b"),"ab");
+    }
+
+     */
+
+
+
+}
